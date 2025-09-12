b254200eae917e1869363de04127f3c3694278a3
Sofía Alejandra Quintana Gutiérrez <39326905+SofiaQuintana@users.noreply.github.com>
2025-09-12 02:11:00 -0600

    feat: added get genders endpoint (#5)

--- src/users/entities/gender.entity.ts
+++ src/users/entities/gender.entity.ts
@@ -1,13 +1,13 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'gender' })
 export class Gender {
+  @ApiProperty({ example: 1, description: 'The unique identifier of the gender' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: 'Masculino', description: 'The name of the gender' })
   @Column()
   name: string;
-
-  @Column({ nullable: true })
-  description: string;
 }
